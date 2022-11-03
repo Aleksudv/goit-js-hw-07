aa8c4ad82a77874c1de4cbde184916c84268a04b
Oleksandr Udovenko <107849168+Aleksudv@users.noreply.github.com>
2022-11-03 18:11:18 +0200

    variant 1.1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".js-gallery");
+
+const markup = galleryItems.map(({
+    preview,
+    original,
+    description
+}) => `<a class="gallery__item"
+    href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+</a>`).join('');
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionsDelay: 250, });
